4b7d862dc69271afd5521726b65beb8ffcf18997
Enes <enesvelovic@gmail.com>
2023-09-13 13:42:05 -0400

    Complete Part 1

---
+++ data/skill-data.js
@@ -0,0 +1,11 @@
+const skills = [
+  {text: 'JavaScript', done: true, _id: 1234567},
+  {text: 'TypeScript', done: false, _id: 1234568},
+  {text: 'Python', done: false, _id: 1234569},
+  {text: 'C++', done: false, _id: 12345670},
+  {text: 'Unreal Engine', done: false, _id: 12345671},
+]
+
+export { 
+	skills
+}
